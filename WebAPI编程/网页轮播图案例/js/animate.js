97089ac034cf8186069e022fe2d54a71fd55670c
Great Coder <cao15370163082@gmail.com>
2021-03-10 10:23:59 +0800

    提交完整版网页轮播图代码

---
+++ WebAPI编程/网页轮播图案例/js/animate.js
@@ -0,0 +1,17 @@
+function animate(obj, target, callback) {
+    clearInterval(obj.timer);
+    obj.timer = setInterval(() => {
+        if (obj.offsetLeft == target) {
+            clearInterval(obj.timer)
+
+            if (callback) {
+                callback();
+            }
+        }
+        var step = (target - obj.offsetLeft) / 20;
+
+        //这里还需要注意的是，就是如果我们是从高数值移动到低数值如果使用向上取整，会出现移动精度偏差，所以我们需要在负数时向下取整数
+        step = step > 0 ? Math.ceil(step) : Math.floor(step);
+        obj.style.left = obj.offsetLeft + step + 'px';
+    }, 10);
+};
--- WebAPI编程/网页轮播图案例/js/index.js
+++ WebAPI编程/网页轮播图案例/js/index.js
@@ -1,6 +1,9 @@
 window.onload = function () {
     var focus = document.querySelector('.focus');
-    var lsArrows = document.querySelectorAll('.arrow-1');
+    var lsArrows = focus.querySelectorAll('.arrow');
+    var arrwo_r = focus.querySelector('.arrow-r');
+    var arrwo_l = focus.querySelector('.arrow-l');
+
 
     var ol_root = document.querySelector('.circle');
     for (var i = 0; i < ol_root.length; i++) {
@@ -12,11 +15,115 @@ window.onload = function () {
         for (let i = 0; i < lsArrows.length; i++) {
             lsArrows[i].style.display = 'block';
         }
+        clearInterval(timer);
+        timer = null;
     });
 
     focus.addEventListener('mouseout', function () {
         for (let i = 0; i < lsArrows.length; i++) {
             lsArrows[i].style.display = 'none';
         }
+        timer = setInterval(function () {
+            arrwo_r.click();
+        }, 2000);
     });
+
+
+    //动态生成小圆圈
+    var ul = focus.querySelector('ul');
+    var ol = focus.querySelector('.circle');
+    var focusWidth = focus.offsetWidth + 1;
+
+
+    //还需要设置一些 li index 项目
+    for (var i = 0; i < ul.children.length; i++) {
+        //创建一个小li
+        var li = document.createElement('li');
+        li.setAttribute('index', i);
+        li.innerText = '';
+        ol.appendChild(li);
+
+        //我们在添加小圆圈的时候绑定 点击事件 搞一个排他思想
+        li.addEventListener('click', function () {
+            for (var j = 0; j < ol.children.length; j++) {
+                var item = ol.children[j].className = "";
+            }
+            this.className = 'current';
+
+            console.log(focusWidth);
+            var index = this.getAttribute('index');
+            num = index;
+            // 点击后我们需要即使更新
+            circle = index;
+            //点击小圆圈开始移动图片 （实则 Ul）
+            animate(ul, -index * focusWidth);
+        });
+    }
+
+    //设置 class name 为 current
+    ol.children[0].className = 'current';
+
+
+    //克隆第一张图片放在ul最后面
+    var first = ul.children[0].cloneNode(true);
+    ul.appendChild(first);
+
+    var num = 0;
+    var circle = 0;
+
+
+    //左右两侧点击事件
+    arrwo_r.addEventListener('click', function () {
+        // 这里我们使用无缝滚动技术 思想返回操作
+        if (num == ul.children.length - 1) {
+            ul.style.left = 0;
+            num = 0;
+        }
+        num++;
+        animate(ul, -num * focusWidth);
+        circle++;
+
+
+        if (circle == ul.children.length - 1) {
+            circle = 0;
+        }
+
+        //先清除其他校小圆圈的current
+        for (var i = 0; i < ol.children.length; i++) {
+            ol.children[i].className = '';
+        }
+
+        ol.children[circle].className = 'current';
+    });
+
+    arrwo_l.addEventListener('click', function () {
+        if (num == 0) {
+            num = ul.children.length - 1;
+            ul.style.left = -num * focusWidth + 'px';
+        }
+        num--;
+        //如何设施 蛋清 current 降级
+        animate(ul, -num * focusWidth);
+
+        circle--;
+
+        circle = circle < 0 ? ol.children.length - 1 : circle;
+
+        circleChange();
+
+    });
+
+    function circleChange() {
+        for (var i = 0; i < ol.children.length; i++) {
+            ol.children[i].className = '';
+        }
+
+        console.log(circle);
+        ol.children[circle].className = 'current';
+    }
+
+    // 自动播放轮播图
+    var timer = setInterval(function () {
+        arrwo_r.click();
+    }, 2000);
 }
