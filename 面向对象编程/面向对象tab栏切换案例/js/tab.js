547e220fab41d0e1cf097290ee94ada4afab80c2
Great Coder <cao15370163082@gmail.com>
2021-03-18 08:52:01 +0800

    完成面向对象tab栏案例

--- 面向对象编程/面向对象tab栏切换案例/js/tab.js
+++ 面向对象编程/面向对象tab栏切换案例/js/tab.js
@@ -5,16 +5,21 @@ class Tab {
         //获取元素
         this.main = document.querySelector(id);
         this.ul = this.main.querySelector('.fisrstnav ul:first-child');
+        // this.remove = this.main.querySelectorAll('.icon-guanbi');
         //set add div to add addTab Method
         this.add = this.main.querySelector('.tabadd')
         this.fsection = this.main.querySelector('.tabscon');
         this.init();
     }
 
-    //获取所有的小li和section
+    //在每次更新后动态的获取对应的元素
     updateNode() {
         this.lis = this.main.querySelectorAll('li');
         this.sections = this.main.querySelectorAll('section');
+        //获取所有的关闭按钮
+        this.remove = this.main.querySelectorAll('.icon-guanbi');
+        //获取所有的span
+        this.spans = this.main.querySelectorAll('.fisrstnav li span:first-child');
     }
 
     init() {
@@ -24,6 +29,12 @@ class Tab {
         for (var i = 0; i < this.lis.length; i++) {
             this.lis[i].index = i;
             this.lis[i].onclick = this.toggleTag;
+
+            //调用删除tab方法
+            this.remove[i].onclick = this.removeTab;
+            //使用ondblclick 添加元素双击事件
+            this.spans[i].ondblclick = this.editTab;
+            this.sections[i].ondblclick = this.editTab;
         }
     }
 
@@ -61,14 +72,51 @@ class Tab {
         that.init();
     }
 
-    //删除功能
-    removeTab() {
+    //删除功能 
+    removeTab(e) {
+        //因为关闭按钮点击事件，但是父亲li也有点击事件，所以需要阻止冒泡
+        e.stopPropagation();
+        //获取到当前的索引值
+        var index = this.parentNode.index;
+        that.lis[index].remove();
+        that.sections[index].remove();
+        that.init();
 
+        if (document.querySelector('.liactive')) return;
+
+        //当我们删除了选中状态的li的时候，让它前一个li处于选定状态
+        index--;
+        that.lis[index] && that.lis[index].click();
     }
 
     //修改功能
     editTab() {
+        var str = this.innerHTML;
+        //双击禁止选定文字 添加这行代码就可以思想上级不选中文字的效果
+        window.getSelection ? window.getSelection().removeAllRanges() : document.getSelection.empty();
+        // alert(11);
 
+        //添加一个input表单
+        this.innerHTML = '<input type="text" />';
+        var input = this.children[0];
+        input.value = str;
+        //这行代码可以让input中的文字处于选中状态
+        input.select();
+
+        //当输入框是失去焦点的时候
+        input.onblur = function () {
+            //完成对显示span的赋值操作
+            this.parentNode.innerHTML = this.value;
+        };
+
+        //按下回车，也可以让输入的内容添加到指定的内容中
+        input.onkeyup = function(e)
+        {
+            if(e.keyCode === 13)
+            {
+                this.blur();
+            }
+        }
     }
 }
 
